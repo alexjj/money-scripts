9d24c8eae41012f9b27dcf8996e5af0553efce42
Alex Johnstone <alexjj@gmail.com>
2016-07-20 16:42:15 -0700

    Update csvtobeancount.py

--- csvtobeancount.py
+++ csvtobeancount.py
@@ -37,3 +37,40 @@ def make_beancount_string(date, payee, amount):
 def write_beancount_file(output, lines):
     with open(output, 'w') as o:
         o.writelines(lines)
+
+
+
+### Probably use pandas
+
+import pandas as pd
+
+citi_csv = 'MC_709_CURRENT_VIEW.CSV'
+beancount_account = 'Liabilities:US:Citi'
+output_file = 'citi.beancount'
+
+# read csv
+df = pd.read_csv(citi_csv, encoding = "ISO-8859-1", thousands=',')
+
+# Drop unnecessary columns
+
+df = df.drop(['Status'], axis=1)
+
+# Remove \n
+
+df['Description'] = df['Description'].str.replace('\n','')
+
+# Move credits to negative debits
+df['Debit'] = df['Debit'].convert_objects(convert_numeric=True)
+df['Credit'] = df['Credit'].convert_objects(convert_numeric=True)
+df.Credit = df.Credit * -1
+df['Debit'] = df['Credit'].where(df['Credit']<0,other=df['Debit'])
+df = df.drop(['Credit'], axis=1)
+
+# Date format
+df['Date'] = df['Date'].convert_objects(convert_dates='coerce')
+
+# Check
+print(df)
+
+#xacts = df.values.tolist()
+#print(xacts)
